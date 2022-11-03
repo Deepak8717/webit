6d54a542b2ab91e6186cf474a104dc5741f3a681
Deepak8717 <sendit2deepak@gmail.com>
2022-11-03 22:05:47 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Banner from "./Banner";
 import MeassurementToOurSuccess from "./MeasurementToOurSuccess";
+import Industries from "./Industries";
 function App() {
   return (
     <div className="App">
       <Banner />
       <MeassurementToOurSuccess />
+      <Industries />
     </div>
   );
 }
---
+++ src/Industries.jsx
@@ -0,0 +1,45 @@
+const Industries = () => {
+  return (
+    <div className="industries-container  measurement-container">
+      <div className="heading-container">
+        <h1 className="heading">We are Working with these industries</h1>
+      </div>
+
+      <div className="grid-container">
+        <div className="grid-item">
+          <div className="icon media"></div>
+          <div className="text">Media & Entertainment</div>
+        </div>
+        <div className="grid-item">
+          <div className="icon healthcare"></div>
+          <div className="text">health care & wellness</div>
+        </div>
+        <div className="grid-item">
+          <div className="icon hospitality"></div>
+          <div className="text">hospitality & real estate</div>
+        </div>
+        <div className="grid-item">
+          <div className="icon learning"></div>
+          <div className="text">Retail & E Commerce</div>
+        </div>
+        <div className="grid-item">
+          <div className="icon travel"></div>
+          <div className="text">travel & transport</div>
+        </div>
+        <div className="grid-item">
+          <div className="icon mobile"></div>
+          <div className="text">mobile & telecom</div>
+        </div>
+        <div className="grid-item">
+          <div className="icon it"></div>
+          <div className="text">it & ites</div>
+        </div>
+        <div className="grid-item">
+          <div className="icon agritech"></div>
+          <div className="text">agritech</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Industries;
